55d3a34ece60e7770da9f5140ba648e35ee4c600
Batyr Nuryyev <b.g.nuryyev@gmail.com>
2017-10-05 23:42:11 -0600

    adds deq js for the MIT tech review

---
+++ mit-tech-review-articles/deq.js
@@ -0,0 +1,12 @@
+/* Removes blocking section */
+function removeBlockingSection() {
+    
+    /* Removes blocking section */
+    document.getElementsByClassName("incognito-wall-shown")[0].classList.remove("incognito-wall-shown");
+    
+    return;
+}
+
+
+/* Invoke the function that will do the necessary work to let you in! */  
+removeBlockingSection(); 
